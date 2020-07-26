ee04d30623ee26de866ca25945627b34ab3bc36a
Noah B Johnson <noah@noahbjohnson.net>
2020-07-26 14:30:53 -0500

    lint

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,65 +1,64 @@
 import * as React from 'react'
-import {useState} from 'react'
-import {Button} from 'rsuite'
+import { useState } from 'react'
+import { Button } from 'rsuite'
 import Dropzone from 'react-dropzone'
-import {uploadFile} from "../api";
+import { uploadFile } from '../api'
 
 export default function () {
-    const [uploadQueue, setUploadQueue] = useState<File[]>([])
+  const [uploadQueue, setUploadQueue] = useState<File[]>([])
 
+  const uploadFiles = async (): Promise<void> => {
+    await Promise.all(uploadQueue.map(file => {
+      return uploadFile(file).then(() => setUploadQueue(uploadQueue.filter((f: File): boolean => {
+        return file.name !== f.name
+      })))
+    }))
+    hide()
+  }
 
-    const uploadFiles = async (): Promise<void> => {
-        await Promise.all(uploadQueue.map(file => {
-            return uploadFile(file).then(() => setUploadQueue(uploadQueue.filter((f: File): boolean => {
-                return file.name !== f.name
-            })))
-        }))
-        hide()
+  const addOne = (file: File): void => {
+    if (uploadQueue.map(f => f.name).includes(file.name)) {
+      console.warn('duplicate file detected')
+    } else {
+      uploadQueue.push(file)
+      setUploadQueue(uploadQueue.map(f => f))
     }
+  }
 
-    const addOne = (file: File): void => {
-        if (uploadQueue.map(f => f.name).includes(file.name)) {
-            console.warn('duplicate file detected')
-        } else {
-            uploadQueue.push(file)
-            setUploadQueue(uploadQueue.map(f => f))
-        }
+  const addToQueue = (files: File | File[]): void => {
+    if (Array.isArray(files)) {
+      files.forEach(addOne)
+    } else {
+      addOne(files)
     }
+    setUploadQueue(uploadQueue)
+  }
 
-    const addToQueue = (files: File | File[]): void => {
-        if (Array.isArray(files)) {
-            files.forEach(addOne)
-        } else {
-            addOne(files)
-        }
-        setUploadQueue(uploadQueue)
-    }
+  const hide = () => {
+    setTimeout(() => setUploadQueue([]), 500)
+  }
 
-    const hide = () => {
-        setTimeout(() => setUploadQueue([]), 500)
-    }
-
-    return (
-        <div className="App">
-            <Dropzone onDrop={(acceptedFiles) => {
-                console.log('file dropped')
-                addToQueue(acceptedFiles)
-            }} accept={'.zip'}>
-                {({getRootProps, getInputProps}) => (
-                    <section>
-                        <div {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            <h1>Drag 'n' drop some files here, or click to select files</h1>
-                        </div>
-                    </section>
-                )}
-            </Dropzone>
-            {
-                uploadQueue.map((file, key) => {
-                    return <p>{file.name}</p>
-                })
-            }
-            <Button onClick={uploadFiles}>Add Mod Files</Button>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Dropzone onDrop={(acceptedFiles) => {
+        console.log('file dropped')
+        addToQueue(acceptedFiles)
+      }} accept={'.zip'}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <h1>Drag 'n' drop some files here, or click to select files</h1>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+      {
+        uploadQueue.map((file, key) => {
+          return <p>{file.name}</p>
+        })
+      }
+      <Button onClick={uploadFiles}>Add Mod Files</Button>
+    </div>
+  )
 }
